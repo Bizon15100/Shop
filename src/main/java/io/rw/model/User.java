02f618a38584333df4735fd65ee691de99e80f8c
Bizon15100 <rafal15100@gmail.com>
2020-11-22 18:20:06 +0000

    User Class prepared

--- src/main/java/io/rw/model/User.java
+++ src/main/java/io/rw/model/User.java
@@ -1,4 +1,36 @@
 package io.rw.model;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String login;
+    private String password;
+    private String email;
+
+
+    public User(String login, String password, String email) {
+        this.login = login;
+        this.password = password;
+        this.email = email;
+    }
+
+    public User() {
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", login='" + login + '\'' +
+                ", password='" + password + '\'' +
+                ", email='" + email +
+                '}';
+    }
+
 }
